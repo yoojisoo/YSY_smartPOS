21d4287c0511fa458cf5926009150467d2d0c491
신민경 <mnew2m@gmail.com>
2022-05-02 18:39:13 +0900

    로그인 회원가입 폼 수정

--- src/vue-front-ysy/src/components/SignIn.vue
+++ src/vue-front-ysy/src/components/SignIn.vue
@@ -1,57 +1,49 @@
 <template>
     <v-main>
         <v-container 
-            style="position: relative; top: 20%;"
+            style="position: relative;"
             v-show="isLogin"
+            fill-height fluid
         >
-            <v-card class="mx-auto" max-width="400" height="350">
-                <v-row style="margin-top: 60px">
-                    <v-col cols="12">
-                        <!-- <v-form> -->
-                            <div class="mx-3">
-                                <v-card-title class="text-h5">로그인</v-card-title>
-                                <div class="mx-1">
-                                    ID
-                                    <v-text-field v-model="user_id" type="text" placeholder="ID" />
-                                    Password
-                                    <v-text-field v-model="user_pw" type="password" placeholder="Password" />
-                                </div>
-                                <v-divider class="mx-1"></v-divider>
-                                <v-card-actions>
-                                    <v-btn color="orange" @click="register">회원가입</v-btn>
-                                    <v-btn color="orange" @click="loginSubmit">로그인</v-btn>
-                                </v-card-actions>
-                            </div>
-                        <!-- </v-form> -->
-                    </v-col>
-                </v-row>
-            </v-card>
+            <v-row align="center" justify="center" style="height:100vh" dense>
+                <v-col cols="12" lg="6" md="6" class="grey lighten-2 fill-height d-flex flex-column justify-center align-center">
+                    <v-card flat height="400px" width="500px">
+                        <v-card-title class="text-h5">로그인</v-card-title>
+                        <v-card-content>
+                            <v-text-field label="ID" v-model="user_id" type="text" />
+                            <v-text-field label="Password" v-model="user_pw" type="password" />
+                        </v-card-content>
+                        <v-card-actions>
+                            <v-btn color="orange" @click="registBtn">회원가입</v-btn>
+                            <v-btn color="orange" @click="loginSubmit">로그인</v-btn>
+                        </v-card-actions>
+                    </v-card>
+                </v-col>
+            </v-row>
         </v-container>
         <v-container 
-            style="position: relative; top: 20%;"
+            style="position: relative;"
             v-show="isRegister"
+            fill-height fluid
         >
-            <v-card flat class="mx-auto" max-width="400" height="350">
-                <v-row style="margin-top: 60px">
-                    <v-col cols="12">
-                        <v-form>
-                            <div class="mx-3">
-                                <v-card-title class="text-h5">회원가입</v-card-title>
-                                <div class="mx-1">
-                                    ID
-                                    <v-text-field v-model="user_id" type="text" placeholder="ID" />
-                                    Password
-                                    <v-text-field v-model="user_pw" type="password" placeholder="Password" />
-                                </div>
-                                <v-card-actions>
-                                    <v-btn color="primary" @click="login">취소</v-btn>
-                                    <v-btn color="primary" @click="registerSubmit">회원가입</v-btn>
-                                </v-card-actions>
-                            </div>
-                        </v-form>
-                    </v-col>
-                </v-row>
-            </v-card>
+            <v-row align="center" justify="center" style="height:80vh" dense>
+                <v-col cols="12" lg="6" md="6" class="grey lighten-2 fill-height d-flex flex-column justify-center align-center">
+                    <v-card flat height="400px" width="500px">
+                        <v-card-title class="text-h5">회원가입</v-card-title>
+                        <v-card-content>
+                            <v-text-field label="ID" v-model="user_id" type="text" />
+                            <v-text-field label="Password" v-model="user_pw" type="password"/>
+                            <v-text-field label="Name" v-model="user_name" type="text" />
+                            <v-text-field label="Phone Number" v-model="user_phone" type="text"/>
+                            <v-text-field label="E-mail" v-model="user_email" type="text" />
+                        </v-card-content>
+                        <v-card-actions>
+                            <v-btn color="primary" @click="cancelBtn">취소</v-btn>
+                            <v-btn color="primary" @click="registerSubmit">회원가입</v-btn>
+                        </v-card-actions>
+                    </v-card>
+                </v-col>
+            </v-row>
         </v-container>
     </v-main>
 </template>
@@ -62,6 +54,9 @@ export default {
         return {
             user_id: "",
             user_pw: "",
+            user_name: "",
+            user_phone: "",
+            user_email: "",
             isLogin: true,
             isRegister: false,
             register_id: "",
