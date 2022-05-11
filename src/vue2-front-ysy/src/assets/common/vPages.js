7cfd8cbac377a56314df8f902fd8dab3afe48342
신민경 <mnew2m@gmail.com>
2022-05-11 18:01:16 +0900

    /ysy/com 폴더 삭제

--- src/vue2-front-ysy/src/assets/common/vPages.js
+++ src/vue2-front-ysy/src/assets/common/vPages.js
@@ -11,11 +11,11 @@ const pages = [
   },
   {
     path: "/403",
-    component: () => import("@/views/ysy/com/error/NotFound.vue"),
+    component: () => import("@/views/error/NotFound.vue"),
   }, // 권한 없는 페이지 - 페이지 생성하고나서 변경하기
   {
     path: "/404",
-    component: () => import("@/views/ysy/com/error/NotFound.vue"),
+    component: () => import("@/views/error/NotFound.vue"),
   }, // 정의되지 않은 나머지 모든 페이지들 - 404 페이지로 redirect
   {
     path: "/:pathMatch(.*)*",
