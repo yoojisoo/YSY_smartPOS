30ddc6b3d329568fae5b9e08af8a38b2ec07558b
yoonjungho <clubbboy@naver.com>
2022-05-18 18:33:20 +0900

    충돌

--- src/main/java/com/ysy/jwt/auth/model/JoinModel.java
+++ src/main/java/com/ysy/jwt/auth/model/JoinModel.java
@@ -13,4 +13,5 @@ public class JoinModel {
 	private String username;
 	private String password;
 	private String name;
+	private String email;
 }
--- src/main/java/com/ysy/jwt/auth/service/YsyUserMstService.java
+++ src/main/java/com/ysy/jwt/auth/service/YsyUserMstService.java
@@ -1,5 +1,9 @@
 package com.ysy.jwt.auth.service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -10,9 +14,10 @@ import com.ysy.common.SysEnum.enumGrps;
 import com.ysy.common.YsyUtil;
 import com.ysy.jwt.auth.entity.YsyBizMst;
 import com.ysy.jwt.auth.entity.YsyGrpMst;
-import com.ysy.jwt.auth.entity.YsyUserMst;
 import com.ysy.jwt.auth.entity.YsyGrpMst.GrpPK;
+import com.ysy.jwt.auth.entity.YsyUserMst;
 import com.ysy.jwt.auth.model.JoinModel;
+import com.ysy.jwt.auth.repository.YsyBizMstRepository;
 import com.ysy.jwt.auth.repository.YsyGrpMstRepository;
 import com.ysy.jwt.auth.repository.YsyUserMstRepository;
 
@@ -23,15 +28,13 @@ public class YsyUserMstService {
 	@Autowired
 	private YsyUserMstRepository ysyUserRepository;
 	@Autowired
-	private YsyBizMstService ysyBizService;
+	YsyBizMstService ysyBizService;
 	@Autowired
-	private YsyGrpMstService ysyGrpService;
-
+	YsyGrpMstService ysyGrpService;
 	@Autowired
-	private YsyGrpMstRepository ysyGrpMstRepository;
-	
+	YsyGrpMstRepository ysyGrpRepository;
 	@Autowired
-	private YsyUtil util;
+	YsyBizMstRepository ysyBizRepository;
 	
 	/** user 등록 */
 	@Transactional
@@ -43,9 +46,11 @@ public class YsyUserMstService {
 		
 		try 
 		{
-			if (!util.isNullAndEmpty(joinModel.getUsername())
-			 && !util.isNullAndEmpty(joinModel.getPassword())
-			 && !util.isNullAndEmpty(joinModel.getName())) {
+			if (!YsyUtil.isNullAndEmpty(joinModel.getBizCd())
+			 && !YsyUtil.isNullAndEmpty(joinModel.getUsername())
+			 && !YsyUtil.isNullAndEmpty(joinModel.getPassword())
+			 && !YsyUtil.isNullAndEmpty(joinModel.getName())
+			 && !YsyUtil.isNullAndEmpty(joinModel.getEmail())) {
 
 				/** 0. 유저 존재 확인 (존재하면 메세지 return / 존재하지 않으면 다음 단계) */
 				if (isUser(joinModel.getUsername())) return "error : user 존재";
@@ -53,37 +58,56 @@ public class YsyUserMstService {
 				// bizCd와 Default Group ID 셋팅
 				String bizCd = joinModel.getBizCd();
 				enumGrps grpId = SysEnum.enumGrps.ROLE_TEMP_USER;
+				List<enumGrps> enumTemp = Stream.of(SysEnum.enumGrps.values()).collect(Collectors.toList());
+				int enumRoleNum;
 				
 				YsyBizMst ysyBizMst = new YsyBizMst();
 				YsyGrpMst ysyGrpMst = new YsyGrpMst();
 				
 				/** 1. bizCd 존재 확인 */
 				if (!ysyBizService.isBizCd(bizCd)) {
+					enumRoleNum = 1;
+					
 					/** 존재하지 않음 -> bizCd가 존재하지 않으면 Default grpId도 없기 때문에 둘 다 새로 등록해줌 */
 					ysyBizMst = YsyBizMst.builder().bizCd(bizCd).bizNm("").useYn("Y").delYn("N").build(); // biz 등록
+					ysyBizRepository.save(ysyBizMst);
 					
-					GrpPK grpPK = new GrpPK(grpId, bizCd); // grp 등록에 쓸 Pk 정보를 넣어줌
-					ysyGrpMst = YsyGrpMst.builder().grpPK(grpPK).grpNm("").useYn("Y").build(); // grp 등록
+					for(enumGrps enumRole : enumTemp) {
+						GrpPK grpPK = new GrpPK(enumRole, bizCd); // grp 등록에 쓸 Pk 정보를 넣어줌
+						ysyGrpMst = YsyGrpMst.builder()
+								.grpPK(grpPK)
+								.ysyBizMst(ysyBizMst)
+								.grpNm("")
+								.useYn("Y")
+								.levelId(enumRoleNum)
+								.build(); // grp 등록
+						
+						ysyGrpRepository.save(ysyGrpMst);
+						enumRoleNum++;
+					}
 					
 					if (!ysyBizService.createBiz(ysyBizMst)) {
 						return "회사코드가 있습니다.";
 					}
-				} else {
-					/** 존재함 -> bizCd가 존재하면 Default grpId도 있기 때문에 Group 정보를 가져옴 */
-					ysyGrpMst = ysyGrpService.getGrpData(bizCd, grpId).get();
 				}
+//				else {
+					/** 존재함 -> bizCd가 존재하면 Default grpId도 있기 때문에 Group 정보를 가져옴 */
+					ysyBizMst = ysyBizRepository.findById(bizCd).get();
+					ysyGrpMst = ysyGrpService.getGrpData(bizCd, grpId).get();
+//				}
 				
 				/** 2. 최종 저장 양식은 YsyUserMst라서 정보를 옮겨 담아줌 */
 				YsyUserMst ysyUser = YsyUserMst.builder()
 						.username(joinModel.getUsername())
 						.password(bCryptPasswordEncoder.encode(joinModel.getPassword()))
 						.name(joinModel.getName())
+						.email(joinModel.getEmail())
 						.ysyGrpMst(ysyGrpMst)
 						.build();
 
 				ysyUserRepository.save(ysyUser);
 				
-				return "ok -> login page move!";
+				return "ok";
 			} else {
 				return "error : Id or password or name is empty!";
 			}
