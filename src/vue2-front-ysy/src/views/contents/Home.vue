ec1a4079ab007620df8e77ffb502ad6a890e6b20
신민경 <mnew2m@gmail.com>
2022-06-03 18:22:20 +0900

    Home Layout 변경

--- src/vue2-front-ysy/src/views/contents/Home.vue
+++ src/vue2-front-ysy/src/views/contents/Home.vue
@@ -10,15 +10,7 @@
 		<v-main>
 			<v-container fluid pa-0 ma-0 style="background-color: lightgray">
 				<v-row justify="center" no-gutters>
-					<v-col
-						cols="12"
-						xs="12"
-						sm="12"
-						md="8"
-						lg="8"
-						xl="8"
-						style="background-color: lightslategray"
-					>
+					<v-col cols="12" md="8" lg="8" xl="8" style="background-color: lightslategray">
 						<v-row justify="center" no-gutters style="height: inherit">
 							<v-col :cols="filterCols" class="hidden-xs-only">
 								<v-card
@@ -30,40 +22,27 @@
 								></v-card>
 							</v-col>
 							<v-col :cols="mainCols">
-								<!-- <v-row justify="center" no-gutters>
-                  <v-col align-self="center" class="hidden-xs-only">
-                    <v-btn
-                      class="ma-2 hidden-xs-only"
-                      @click="colsChange"
-                    >
-                      web filter
-                    </v-btn>
-                  </v-col>
-                  <v-col align-self="center" class="hidden-sm-and-up">
-                    <v-btn
-                      class="ma-2"
-                      @click="colsChange"
-                    >
-                      mobile filter 
-                    </v-btn>
-                  </v-col>
-                </v-row> -->
+								<!--<v-row justify="center" no-gutters>
+									<v-col align-self="center" class="hidden-xs-only">
+										<v-btn
+										class="ma-2 hidden-xs-only"
+										@click="colsChange"
+										>
+										web filter
+										</v-btn>
+									</v-col>
+									<v-col align-self="center" class="hidden-sm-and-up">
+										<v-btn
+										class="ma-2"
+										@click="colsChange"
+										>
+										mobile filter 
+										</v-btn>
+									</v-col>
+								</v-row>-->
 								<v-row justify="center" no-gutters>
 									<v-col clos="12">
-										<v-card color="#385F73" dark>
-											<v-card-title class="text-h5">
-												Unlimited music now
-											</v-card-title>
-
-											<v-card-subtitle
-												>Listen to your favorite artists and albums whenever
-												and wherever, online and offline.</v-card-subtitle
-											>
-
-											<v-card-actions>
-												<v-btn text> Listen Now </v-btn>
-											</v-card-actions>
-										</v-card>
+										<pageHistory :pageNameKo="pageNameKo" />
 									</v-col>
 								</v-row>
 								<v-row align="start" justify="center" no-gutters>
@@ -92,6 +71,7 @@ import mainSystemBar from '@/components/header/TheSystemBar.vue';
 import mainHeader from '@/components/header/TheHeader.vue';
 import mainFooter from '@/components/TheFooter.vue';
 import ysyGrid from '@/components/YsyGrid.vue';
+import pageHistory from '@/components/PageHistory.vue';
 
 export default {
 	components: {
@@ -99,6 +79,7 @@ export default {
 		mainHeader,
 		mainFooter,
 		ysyGrid,
+		pageHistory,
 	},
 	data() {
 		return {
@@ -106,6 +87,7 @@ export default {
 			mainCols: 12,
 			filterShow: false,
 			pageName: 'home',
+			pageNameKo: '메인페이지',
 			config: {
 				headers: {
 					access_token: this.$store.state.authStore.loginData.userToken,
