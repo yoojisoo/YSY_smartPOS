98bd2f5e0247ce805f7ee633d85836937fd86648
신민경 <mnew2m@gmail.com>
2022-06-07 09:06:29 +0900

    로그인 상태 체크 잠깐 주석 ~!

--- src/vue2-front-ysy/src/router/index.js
+++ src/vue2-front-ysy/src/router/index.js
@@ -4,13 +4,13 @@ import store from 'vuex';
 
 /** 로그인 상태를 체크해서
  * 이동가능한 페이지인지 확인 */
-const isLoginChk = () => (to, from, next) => {
-	if (store.mapGetters(['isLogin'])) {
-		alert('로그인 상태로는 이동하실 수 없는 페이지입니다.\n로그아웃을 먼저 진행해주세요.');
-		next('/');
-	}
-	return next();
-};
+//const isLoginChk = () => (to, from, next) => {
+//	if (store.mapGetters(['isLogin'])) {
+//		alert('로그인 상태로는 이동하실 수 없는 페이지입니다.\n로그아웃을 먼저 진행해주세요.');
+//		next('/');
+//	}
+//	return next();
+//};
 
 Vue.use(VueRouter);
 // vMenus.getMenuList();
@@ -47,13 +47,13 @@ const routes = [
 		path: '/signIn',
 		name: 'signIn',
 		component: () => import('../views/sign/SignIn.vue'),
-		beforeEnter: isLoginChk(),
+		//beforeEnter: isLoginChk(),
 	},
 	{
 		path: '/signUp',
 		name: 'signUp',
 		component: () => import('../views/sign/SignUp.vue'),
-		beforeEnter: isLoginChk(),
+		//beforeEnter: isLoginChk(),
 	},
 
 	// 에러페이지
