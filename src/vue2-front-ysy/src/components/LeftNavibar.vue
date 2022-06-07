767c767ecfa585bb098313847312bf67c107adda
yoonjungho <clubbboy@naver.com>
2022-06-07 09:14:36 +0900

    ,,

---
+++ src/vue2-front-ysy/src/components/LeftNavibar.vue
@@ -0,0 +1,22 @@
+<template>
+    <v-app>
+        <v-navigation-drawer
+            v-model="drawer"
+            app
+        >
+        </v-navigation-drawer>
+    </v-app>
+</template>
+
+<script>
+export default {
+    name: 'left_navi',
+     data: () => ({
+      drawer: null,
+    }),
+}
+</script>
+
+<style>
+
+</style>
