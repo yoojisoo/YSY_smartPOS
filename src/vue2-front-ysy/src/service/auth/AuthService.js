f98f119ccd5a930999658a2c05d34df918e74550
yoonjungho <clubbboy@naver.com>
2022-06-13 17:11:36 +0900

    code 정리

--- src/vue2-front-ysy/src/service/auth/AuthService.js
+++ src/vue2-front-ysy/src/service/auth/AuthService.js
@@ -1,19 +1,18 @@
 import store from '@/store/index';
 import jwt_decode from 'jwt-decode';
-import router from "@/router/index"
-import Axios from 'axios';
+import axios from 'axios';
 
 
 
 class AuthService{
 
     setLoginData(headers){
-        // Axios.defaults.headers.common['access_token'] = headers.access_token;
+        axios.defaults.headers.common['access_token'] = headers.access_token;
         var decodedHeader_access  = jwt_decode(headers.access_token , { payload: true });
         var decodedHeader_refresh = jwt_decode(headers.refresh_token, { payload: true });
         if(headers.access_token === null || headers.access_token === ""){
-            router.replace('/403');
-            return;
+           
+            return false;
         }
         let payload = {
             user_id: decodedHeader_access.username,
@@ -24,7 +23,7 @@ class AuthService{
             refresh_token_exp: decodedHeader_refresh.exp,
         };
         store.dispatch('setUserInfo', payload);
-        router.replace('/');
+        return true;
     }
 
 }
--- src/vue2-front-ysy/src/views/sign/KakaoLogin.vue
+++ src/vue2-front-ysy/src/views/sign/KakaoLogin.vue
@@ -11,67 +11,34 @@ export default {
 	},
 	data() {
 		return {
-			codes: '',
-			form: {
-				password: '',
-				email: '',
-				uname: '',
-			},
-			show: true,
+			code: '',
 		};
 	},
 	methods: {
 		async kakaoLogin() {
 			console.log("this.code ==========================> "+this.$route.query.code);
-			this.codes = this.$route.query.code;
+			this.code = this.$route.query.code;
 			let params = {
-				code : this.codes,
+				code : this.code,
 				path : "kakao",
 			}
 			await this.$axios.post('/ysy/v1/auth/kakao/setCode' , params)
 				.then(res=>{
 					if(res.data !== "ok"){
 						alert(res.data);
-						 this.$router.replace('/signIn');
+						this.$router.replace('/signIn');
 						return;
 					}
-					this.$axios.defaults.headers.common['access_token'] = res.headers.access_token;
-					authService.setLoginData(res.headers);
+					// this.$axios.defaults.headers.common['access_token'] = res.headers.access_token;
+					let flag = authService.setLoginData(res.headers);
 					console.log(res.data);
+					if(flag) {
+						this.$router.replace('/');
+					}
 				})
 				.catch(error=>{
-
+					this.$router.replace('/403');
 				});
-			// this.getToken();
-		},
-		login() {
-			axios.post('http://localhost:8080/login', this.form).then(res => {
-				if (res.data != null) {
-					document.cookie = `accessToken=${res.data}`;
-					axios.defaults.headers.common['x-access-token'] = res.data;
-					this.$router.push('/');
-				}
-			});
-		},
-		getToken() {
-			axios.get('http://localhost:8080/klogin?authorize_code=' + this.codes).then(res => {
-				this.form.email = res.data.email;
-				this.form.password = res.data.id;
-				if (this.form.password == undefined) {
-					alert('올바르지 못한 접근입니다.');
-					this.$router.push('/');
-				} else {
-					this.login();
-				}
-			});
-		},
-		onSubmit(event) {
-			event.preventDefault();
-			// alert(JSON.stringify(this.form));
-			axios.post('http://localhost:8080/join', this.form).then(res => {
-				console.log(res.status);
-				this.login();
-			});
 		},
 	},
 };
