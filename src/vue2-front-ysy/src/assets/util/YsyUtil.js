7f58ab998e4f54ac011c2e58a935e1c18b800baf
yoonjungho <clubbboy@naver.com>
2022-07-12 09:09:47 +0900

    ysyutil 추가

---
+++ src/vue2-front-ysy/src/assets/util/YsyUtil.js
@@ -0,0 +1,36 @@
+'use strict'
+
+class YsyUtil {
+    isNull = str => {
+        if(str === undefined || str == null ){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    isNullAndEmpty = (str )=>{
+        if(str === undefined || str == null || str.trim() === ""){
+            return true;
+        }else{
+            return false;
+        } 
+    }
+}
+// const util = {
+//     isNull : str => {
+//         if(str === undefined || str == null ){
+//             return true;
+//         }else{
+//             return false;
+//         }
+//     },
+//     isNullAndEmpty : (str )=>{
+//         if(str === undefined || str == null || str.trim() === ""){
+//             return true;
+//         }else{
+//             return false;
+//         } 
+//     }
+// }
+
+export default YsyUtil
