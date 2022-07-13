b5e07d6a936a2c466f18a268dedcc689e3047877
yoonjungho <clubbboy@naver.com>
2022-07-13 11:46:37 +0900

    textfield sample

---
+++ src/vue2-front-ysy/src/components/textFileds/YsyTextField.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-text-field :label="fieldInfo.label"  :prepend-icon="fieldInfo.icon" v-model="fieldInfo.data"></v-text-field>
+</template>
+
+<script>
+/***
+ * props -> fieldInfo
+ *  - label :String
+ *  - icon  :String   ex)mdi-mailbox-up
+ *  - data  :String
+ */
+export default {
+    props: ['fieldInfo'],
+    data() {
+        return {
+          
+        }
+    },
+    methods: {
+    },
+    mounted() {
+        console.log("this.fieldInfo ------->");
+        if(this.fieldInfo.label == undefined || this.fieldInfo.label.trim() === ""){
+            this.fieldInfo.label = "우편번호";//default
+        }
+        if(this.fieldInfo.icon == undefined || this.fieldInfo.icon.trim() === ""){
+            this.fieldInfo.icon = "mdi-mailbox-up";//default
+        }
+        if(this.fieldInfo.data == undefined || this.fieldInfo.data.trim() === ""){
+            this.fieldInfo.data = "";//default
+        }
+        console.log(this.fieldInfo);
+    }
+}
+</script>
+
+<style>
+
+</style>
--- src/vue2-front-ysy/src/components/user/UserAddrEdit.vue
+++ src/vue2-front-ysy/src/components/user/UserAddrEdit.vue
@@ -8,6 +8,7 @@
             <v-btn outlined>수정</v-btn>
         </v-card-title>
         <v-card-text>
+            <ysyTextField :fieldInfo="temInfo"/>
             <v-text-field label="우편번호"  prepend-icon="mdi-mailbox-up" v-model="addr.addrZipCode"></v-text-field>
             <v-text-field label="기본주소"  prepend-icon="mdi-map-marker" v-model="addr.addrDetail"></v-text-field>
             <v-text-field label="상세주소"  prepend-icon="mdi-home-map-marker" v-model="addr.addrEtc"></v-text-field>
@@ -17,18 +18,28 @@
     </v-container>
 </template>
 <script>
+import ysyTextField from '@/components/textFileds/YsyTextField.vue';
 export default {
     props: ['addr'],
     data() {
         return {
             idx : [1, 2, 3, 4, 5],
+            temInfo : {
+                label :"우편번호1",
+                icon :"mdi-mailbox-up",
+                data :"data1"
+            }
         }
+    },
+    components:{
+        ysyTextField
     },
     methods: {
     },
     mounted() {
         console.log("this.addr ------->");
         console.log(this.addr);
+        
     }
 
 }
