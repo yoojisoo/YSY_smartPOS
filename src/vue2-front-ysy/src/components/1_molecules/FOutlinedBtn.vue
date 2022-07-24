207efb8ef0839dae3d649aeabf34cc43ce23f37b
DESKTOP-A3FBMAA\s_plu <s_plus7@naver.com>
2022-07-25 00:15:26 +0900

    커밋 대체 어떻게하는겨..

---
+++ src/vue2-front-ysy/src/components/1_molecules/FOutlinedBtn.vue
@@ -0,0 +1,17 @@
+<template>
+    <!-- <v-btn outlined :height="parsed" @click="event"> -->
+    <v-btn outlined :height="btnHeight" @click="$emit('click', true)">
+        {{ btnText }}
+    </v-btn>
+</template>
+
+<script>
+export default {
+    // props: ['btnText', 'btnHeight'],
+    props: {
+        btnText: String,
+        btnHeight: Number,
+        click: Function
+    },
+}
+</script>
