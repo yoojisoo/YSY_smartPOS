479c14d8511959869512b216586c522544cd0577
mnew2m <mnew2m@gmail.com>
2022-07-28 14:35:49 +0900

    Agile Auto Play 5초 적용

--- src/vue2-front-ysy/src/components/FCarousel.vue
+++ src/vue2-front-ysy/src/components/FCarousel.vue
@@ -1,6 +1,7 @@
 <template>
 	<div class="mt-n8">
-		<agile :initial-slide="0">
+		<!-- auto play 5000 : 5초 간격으로 자동으로 넘어감 -->
+		<VueAgile autoplay :autoplay-speed="5000">
 			<img
 				class="slide"
 				src="https://images.unsplash.com/photo-1506260408121-e353d10b87c7?ixlib=rb-1.2.1&amp;q=85&amp;fm=jpg&amp;crop=entropy&amp;cs=srgb&amp;w=1600&amp;fit=max&amp;ixid=eyJhcHBfaWQiOjE0NTg5fQ"
@@ -13,29 +14,13 @@
 				class="slide"
 				src="https://images.unsplash.com/photo-1524260855046-f743b3cdad07?ixlib=rb-1.2.1&amp;q=85&amp;fm=jpg&amp;crop=entropy&amp;cs=srgb&amp;w=1600&amp;fit=max&amp;ixid=eyJhcHBfaWQiOjE0NTg5fQ"
 			/>
-			<img
-				class="slide"
-				src="https://images.unsplash.com/photo-1526080676457-4544bf0ebba9?ixlib=rb-1.2.1&amp;q=85&amp;fm=jpg&amp;crop=entropy&amp;cs=srgb&amp;w=1600&amp;fit=max&amp;ixid=eyJhcHBfaWQiOjE0NTg5fQ"
-			/>
-			<img
-				class="slide"
-				src="https://images.unsplash.com/photo-1524429656589-6633a470097c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
-			/>
-			<img
-				class="slide"
-				src="https://images.unsplash.com/photo-1426170042593-200f250dfdaf?ixlib=rb-1.2.1&amp;q=85&amp;fm=jpg&amp;crop=entropy&amp;cs=srgb&amp;w=1600&amp;fit=max&amp;ixid=eyJhcHBfaWQiOjE0NTg5fQ"
-			/>
-			<img
-				class="slide"
-				src="https://images.unsplash.com/photo-1503424886307-b090341d25d1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1355&q=80"
-			/>
 			<template slot="prevButton">
 				<i class="fas fa-chevron-left"></i>
 			</template>
 			<template slot="nextButton">
 				<i class="fas fa-chevron-right"></i>
 			</template>
-		</agile>
+		</VueAgile>
 	</div>
 </template>
 
@@ -45,7 +30,7 @@ import { VueAgile } from 'vue-agile';
 export default {
 	name: 'Carousel',
 	components: {
-		agile: VueAgile,
+		VueAgile,
 	},
 };
 </script>
